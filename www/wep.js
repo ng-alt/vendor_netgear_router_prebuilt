c405e6783c1ef445b9720a938645dfc57eeed027
ccc <cadappl@gmail.com>
2017-12-01 13:40:24 +0800

    Import R6400-V1.0.1.32_1.0.23
    Change-Id: I523658bc36212c0ba8e134ab9d8bc3e5d16ac08f

--- www/wep.js
+++ www/wep.js
@@ -17,14 +17,14 @@ function checkKey(evt)
     if ((lastkeypressed != 13) && (lastkeypressed != 8) && ( keyTooBig ))
     {
         keyTooBig = false;
-        alert("<%1484%> " + keysize + " <%1485%>");
+        alert("<%1490%> " + keysize + " <%1491%>");
         chkSize(lastObj); // for NS 6/7
         return false;
     }
     else if ((lastkeypressed != 13) && (lastkeypressed != 8) && ( keyTooBig_an ))
     {
         keyTooBig_an = false;
-        alert("<%1484%> " + keysize_an + " <%1485%>");
+        alert("<%1490%> " + keysize_an + " <%1491%>");
         chkSize_an(lastObj); // for NS 6/7
         return false;
     }
